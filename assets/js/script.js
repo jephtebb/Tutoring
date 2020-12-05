273b9c8b9b8297fc891d5a4cd2403f346bd42737
Jephte Borno <jephteborno@yahoo.com>
2020-12-04 20:50:53 -0500

    modal form added

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,12 @@ var mathImagesSrc = [
         setTimeout('slideShow()', 4000);
     }
     window.onload = slideShow;
+    // Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
