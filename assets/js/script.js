3cfa77540b1beb2f0df3656615ff3eaf88f64107
Jephte Borno <jephteborno@yahoo.com>
2020-12-05 22:00:56 -0500

    add some more images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var mathImagesSrc = [
     './assets/images/slide-show-images/math0.jpg',
-    //'./assets/images/slide-show-images/math1.jpg',
+    './assets/images/slide-show-images/math1.jpg',
     './assets/images/slide-show-images/math2.jpg',
-    //'./assets/images/slide-show-images/math3.jpg',
+    './assets/images/slide-show-images/math3.jpg',
     //'./assets/images/slide-show-images/math4.jpg',
     //'./assets/images/slide-show-images/math5.jpg',
     './assets/images/slide-show-images/math6.jpeg',
